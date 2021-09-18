cf285a3838c2806028702e2663cfd22c1da57346
rogpe <prog_x@hotmail.com>
2021-09-18 17:19:12 -0300

    h2 database

--- dscatalog/backend/src/main/java/dev/heroes/dscatalog/entities/Category.java
+++ dscatalog/backend/src/main/java/dev/heroes/dscatalog/entities/Category.java
@@ -3,9 +3,21 @@ package dev.heroes.dscatalog.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
-public class Category implements Serializable {
+import javax.annotation.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.TableGenerator;
 
+@Entity
+@Table(name = "tb_category")
+public class Category implements Serializable {
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	
